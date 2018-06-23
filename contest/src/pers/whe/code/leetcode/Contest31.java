ab69ff3cde89667c1a09d7cccf251cda12415fd0
wanghongen <whe_bupt@163.com>
2018-06-23 22:51:55 +0800

    576 contest 31

--- contest/src/pers/whe/code/leetcode/Contest31.java
+++ contest/src/pers/whe/code/leetcode/Contest31.java
@@ -2,8 +2,7 @@ package pers.whe.code.leetcode;
 
 import pers.whe.code.model.TreeNode;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Contest31 {
 
@@ -44,6 +43,38 @@ public class Contest31 {
         if (s == null || t == null) return false;
         if (s.val != t.val) return false;
         return dfs(s.left, t.left) && dfs(s.right, t.right);
+    }
+
+    /*
+     * 576. Out of Boundary Paths
+     * 这道题是让找从一个矩阵的一点开始，每次可以上下左右，最多能走N步，
+     * 问有多少条路可以走出去，可以用dfs,在用map记录当前状态，
+     * 递归的定义是：在 i , j 点，最多能走N步，有多少条路。
+     * 1 如果当前在外面，返回1
+     * 2 如果N = 0。但是还在里面，无路可走，反回0
+     * 3 如果到到达过当前状态，直接返回，
+     * 4 遍历每个方向
+     * */
+    public int findPaths(int m, int n, int N, int i, int j) {
+        Map<String, Integer> map = new HashMap<>();
+        return dfs(m, n, N, i, j, map);
+    }
+
+    private int dfs(int m, int n, int N, int i, int j, Map<String, Integer> map) {
+        if (i < 0 || i >= m || j < 0 || j >= n) {
+            return 1;
+        }
+        if (N == 0) return 0;
+        String str = Arrays.toString(new int[]{i, j, N});
+        if (map.containsKey(str)) return map.get(str);
+        int[] dx = new int[]{-1, 1, 0, 0};
+        int[] dy = new int[]{0, 0, -1, 1};
+        int res = 0;
+        for (int k = 0; k < dx.length; k++) {
+            res = (res + dfs(m, n, N - 1, i + dx[k], j + dy[k], map)) % 1000000007;
+        }
+        map.put(str, res);
+        return res;
     }
 
 
