4207a4803f6e19ebd8542c40c00757ee403f3960
wanghongen <whe_bupt@163.com>
2018-06-24 14:44:03 +0800

    odd and even in MathUtil

--- contest/src/pers/whe/code/util/MathUtil.java
+++ contest/src/pers/whe/code/util/MathUtil.java
@@ -2,6 +2,9 @@ package pers.whe.code.util;
 
 public class MathUtil {
 
+    public static final int ODD = 1;
+    public static final int EVEN = 2;
+
     public static int findGCD(int a, int b) {
         return a != 0 ? findGCD(b % a, a) : b;
     }
