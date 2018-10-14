07ac0e0253ccc7e5830c3aae3d8f69736b0cad19
wanghongen <whe_bupt@163.com>
2018-10-14 15:29:21 +0800

    Context 106

---
+++ contest/src/pers/whe/code/leetcode/Context106.java
@@ -0,0 +1,76 @@
+package pers.whe.code.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class Context106 {
+
+    /*
+    * 922. Sort Array By Parity II
+    * */
+    public int[] sortArrayByParityII(int[] a) {
+        List<Integer> o = new ArrayList<>();
+        List<Integer> e = new ArrayList<>();
+        for (int i: a) {
+            if (i % 2 == 0) {
+                o.add(i);
+            } else {
+                e.add(i);
+            }
+        }
+        int[] res = new int[a.length];
+        for (int i = 0; i < a.length; i++) {
+            if (i % 2 == 0) {
+                res[i] = o.get(i / 2);
+            } else {
+                res[i] = e.get(i / 2);
+            }
+        }
+        return res;
+    }
+
+    /*
+    * 921. Minimum Add to Make Parentheses Valid
+    * */
+    public int minAddToMakeValid(String s) {
+        int left = 0, right = 0;
+        for (char c: s.toCharArray()) {
+            if (c == '(') {
+                left++;
+            } else {
+                if (left > 0) {
+                    left--;
+                } else {
+                    right++;
+                }
+            }
+        }
+        return left + right;
+    }
+
+    /*
+    * 923. 3Sum With Multiplicity
+    * */
+    public int threeSumMulti(int[] A, int target) {
+        long[] c = new long[101];
+        for (int a : A) c[a]++;
+        long res = 0;
+        for (int i = 0; i <= 100; i++)
+            for (int j = 0; j <= 100; j++) {
+                int k = target - i - j;
+                if (k > 100 || k < 0) continue;
+                if (i == j && j == k)
+                    res += c[i] * (c[i] - 1) * (c[i] - 2) / 6;
+                else if (i == j && j != k)
+                    res += c[i] * (c[i] - 1) / 2 * c[k];
+                else if (i < j && j < k)
+                    res += c[i] * c[j] * c[k];
+            }
+        return (int)(res % (1e9 + 7));
+    }
+
+
+
+}
--- contest/src/pers/whe/code/work/Main.java
+++ contest/src/pers/whe/code/work/Main.java
@@ -1,10 +1,42 @@
 package pers.whe.code.work;
-import pers.whe.code.model.Interval;
-import pers.whe.code.model.ListNode;
 
-import java.util.*;
+
 
 public class Main {
 
+    /*
+    * 358 Rearrange String k Distance Apart
+    * 用nextV 数组记录 对应字母出现的最早距离，贪心每次优先
+    * 排列数量最大的字母。
+    * */
+   public static String rerange(String str, int k) {
+        int[] count = new int[26];
+        int[] nextV = new int[26];
+        for (char c: str.toCharArray()) {
+            count[c - 'a']++;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            int next = getNext(count, nextV, i);
+            if (next == -1) return "";
+            else {
+                sb.append((char)('a' + next));
+                count[next]--;
+                nextV[next] += k;
+            }
+        }
+        return sb.toString();
+   }
 
+    private static int getNext(int[] count, int[] nextV, int index) {
+        int next = -1;
+        int max = 0;
+        for (int i = 0; i < count.length; i++) {
+            if (count[i] > max && index >= nextV[i]) {
+                max = count[i];
+                next = i;
+            }
+        }
+        return next;
+    }
 }
