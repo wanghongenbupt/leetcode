e449e2df9fd4f90943b1215527bd985c8054076e
wanghongen <whe_bupt@163.com>
2019-01-13 14:43:51 +0800

    add Context119

--- contest/src/pers/whe/code/util/MathUtil.java
+++ contest/src/pers/whe/code/util/MathUtil.java
@@ -8,4 +8,9 @@ public class MathUtil {
     public static int findGCD(int a, int b) {
         return a != 0 ? findGCD(b % a, a) : b;
     }
+
+    /*
+     * %  (a - b) % k = n
+     *    (a + k - (b + k)) % b = n
+     * */
 }
