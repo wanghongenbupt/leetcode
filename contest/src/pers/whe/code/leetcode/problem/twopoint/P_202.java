15bf0f545f98fb26127960896709ec54b13a0802
wanghongen <whe_bupt@163.com>
2019-01-14 12:47:56 +0800

    202. Happy Number

--- contest/src/pers/whe/code/leetcode/problem/twopoint/P_202.java
+++ contest/src/pers/whe/code/leetcode/problem/twopoint/P_202.java
@@ -1,4 +1,27 @@
 package pers.whe.code.leetcode.problem.twopoint;
 
 public class P_202 {
+
+    /*
+    * 202. Happy Number
+    * 快慢指针
+     * */
+    public boolean isHappy(int n) {
+        int fast = n, slow = n;
+        do {
+            fast = compute(compute(fast));
+            slow = compute(slow);
+            if (fast == 1) return true;
+        } while (fast != slow);
+        return false;
+    }
+
+    private int compute(int n) {
+        int res = 0;
+        while (n > 0) {
+            res += (n % 10) * (n % 10);
+            n /= 10;
+        }
+        return res;
+    }
 }
