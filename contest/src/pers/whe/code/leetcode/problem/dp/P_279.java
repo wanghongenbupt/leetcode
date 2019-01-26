248d5ac820b095825e5822ea987ae7a43dae7af1
wanghongen <whe_bupt@163.com>
2019-01-26 10:16:36 +0800

    add 279. Perfect Squares

---
+++ contest/src/pers/whe/code/leetcode/problem/dp/P_279.java
@@ -0,0 +1,19 @@
+package pers.whe.code.leetcode.problem.dp;
+
+public class P_279 {
+
+    /*
+     * 279. Perfect Squares
+     *  dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+     * */
+    public int numSquares(int n) {
+        int[] dp = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            dp[i] = Integer.MAX_VALUE;
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+        return dp[n];
+    }
+}
