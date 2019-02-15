dc36dec54310b6ba37de8b0d8284216ed8d4e094
wanghongen <whe_bupt@163.com>
2019-02-15 13:17:45 +0800

    add 334. Increasing Triplet Subsequence

---
+++ contest/src/pers/whe/code/leetcode/problem/greedy/P_334.java
@@ -0,0 +1,22 @@
+package pers.whe.code.leetcode.problem.greedy;
+
+public class P_334 {
+
+    /*
+    * 334. Increasing Triplet Subsequence
+    *
+     * */
+    public boolean increasingTriplet(int[] nums) {
+        int cur = Integer.MAX_VALUE, cur1 = Integer.MAX_VALUE;
+        for (int num : nums) {
+            if (num <= cur) {
+                cur = num;
+            } else if (num <= cur1) {
+                cur1 = num;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
